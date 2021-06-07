1f4d308bdb3be596ac7532750b13d7c4247f7eea
Fahad-5885 <fhdshmsmoly@gmail.com>
2021-06-07 12:48:21 +0530

    DICT CONCAT 2

--- trail.py
+++ trail.py
@@ -1,7 +1,5 @@
-dic1 = {1:10,2:20}
 dic2 = {3:30,4:40}
 dic3 = {5:50,6:60}
-print(dic1,dic2,dic3)
-dic1.update(dic3)
-dic1.update(dic2)
-print(dic1)
+print(dic2,dic3)
+dic2.update(dic3)
+print(dic2)
